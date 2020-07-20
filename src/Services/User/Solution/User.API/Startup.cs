071fdc7f650a1ff49c13add2ece599c3fe3be799
haristauqir <haristauqir@gmail.com>
2020-07-20 16:13:23 +0100

    Swagger/Db Context added in startup

--- src/Services/User/Solution/User.API/Startup.cs
+++ src/Services/User/Solution/User.API/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +11,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using User.Data.Database;
 
 namespace User.API
 {
@@ -25,7 +29,10 @@ namespace User.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().Services
+            .AddCustomMVC(Configuration)
+            .AddCustomDbContext(Configuration)
+            .AddSwagger(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,10 +49,74 @@ namespace User.API
 
             app.UseAuthorization();
 
+            app.UseSwagger()
+             .UseSwaggerUI(c =>
+             {
+                 c.SwaggerEndpoint("v1/swagger.json", "User.API V1");
+             });
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+          
         }
     }
+
+    public static class CustomExtensionMethods
+  {
+
+     public static IServiceCollection AddCustomMVC(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                    .SetIsOriginAllowed((host) => true)
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+            return services;
+        }
+
+    public static IServiceCollection AddCustomDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+      services.AddDbContext<UserContext>(options =>
+      {
+        options.UseSqlServer(configuration["ConnectionString"],
+            sqlServerOptionsAction: sqlOptions =>
+            {
+            sqlOptions.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
+                sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+        });
+      });
+
+      return services;
+    }
+
+    public static IServiceCollection AddSwagger(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "MicroserviceDemo - User HTTP API",
+                    Version = "v1",
+                    Description = "The User Microservice HTTP API. This is a Data-Driven/CRUD microservice sample",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "M Haris",
+                        Email = string.Empty,
+                        Url = new Uri("https://github.com/haristauqir"),
+                    }
+                });
+            });
+
+            return services;
+
+        }
+  }
 }
