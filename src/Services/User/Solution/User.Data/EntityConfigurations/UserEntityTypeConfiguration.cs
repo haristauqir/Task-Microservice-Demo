89481033b2926147f33f2dc65022fca374d10d95
haristauqir <haristauqir@gmail.com>
2020-07-20 15:35:45 +0100

    First commit

---
+++ src/Services/User/Solution/User.Data/EntityConfigurations/UserEntityTypeConfiguration.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MicroserviceDemo.Services.User.Domain.Entities;
+
+namespace MicroserviceDemo.Services.User.Data.EntityConfigurations
+{
+  public class UserEntityTypeConfiguration : IEntityTypeConfiguration<AppUser>
+  {
+    public void Configure(EntityTypeBuilder<User.Domain.Entities.AppUser> builder)
+    {
+      builder.ToTable("User");
+
+      builder.Property(u => u.Id)
+        .IsRequired()
+        .HasColumnName("PrimaryKey_UserId");
+
+      builder.Property(u => u.FirstName)
+        .IsRequired(true)
+        .HasMaxLength(50);
+
+      builder.Property(u => u.LastName)
+        .IsRequired(true)
+        .HasMaxLength(50);
+
+      builder.Property(u => u.IsActive)
+        .IsRequired(true)
+        .HasDefaultValue(true);
+
+    }
+  }
+}
