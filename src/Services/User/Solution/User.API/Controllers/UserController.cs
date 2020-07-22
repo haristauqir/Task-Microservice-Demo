34e63d12b7f54134179734f9cf7f2ef811eb7c6a
haristauqir <haristauqir@gmail.com>
2020-07-22 11:49:35 +0100

    Added Service project

---
+++ src/Services/User/Solution/User.API/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+namespace User.API.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+
+        }
+
+        
+    }
+}
